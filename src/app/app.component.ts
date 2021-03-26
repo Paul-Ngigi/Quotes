1ed9030046dd1356fca182200d4b8720a864c6bc
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-26 08:18:14 +0300

    display the quotes in a HTML file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,5 @@ import { Quote } from './quote'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: Quote[] = [
-    {id:1,quote:"Never give up"},
-    {id:2,quote:"Aspire to inspire before we expire"},
-    {id:3,quote:"Love for all, hatred for none"},
-    {id:4,quote:"Die with memories, not dreams"},
-    {id:5,quote:"Never regret anything that made you smile"},
-  ]
+  
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[] = [
+    {id:1,quote:"Never give up"},
+    {id:2,quote:"Aspire to inspire before we expire"},
+    {id:3,quote:"Love for all, hatred for none"},
+    {id:4,quote:"Die with memories, not dreams"},
+    {id:5,quote:"Never regret anything that made you smile"},
+  ];
   constructor() { }
 
   ngOnInit(): void {
