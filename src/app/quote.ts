a69d6f2ab67df3c42cc908685f156d3f6be0337c
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-27 12:28:15 +0300

    add the visual representation of the site's structure

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    id!: number;
-    quote!: string;
-    username!: string
-    author!: string
+    // showQuote!: boolean
+    constructor(public id: number, public quote: string, public username: string, public author: string){
+        // this.showQuote = false;
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    {id:1,quote:"One day I will find the right words, and they will be simple.",username:"Paul Ngigi",author:"Jack Kerouac"},
-    {id:2,quote:"Let me live, love, and say it well in good sentences.",username:"Ashley Wanjiru",author:"Sylvia Plath"},
-    {id:3,quote:"The first draft is just you telling yourself the story.",username:"Stephen Kariuki",author:"Terry Pratchett"},
-    {id:4,quote:"Start before you’re ready",username:"Mary Wangui",author:"Steven Pressfield"},
-    {id:5,quote:"If you have no critics, you’ll likely have no success",username:"Joseph Kuria",author:"Malcolm X"},
+    {id:1,quote:"One day I will find the right words, and they will be simple",username:"Paul Ngigi",author:'"Jack Kerouac"'},
+    {id:2,quote:"Let me live, love, and say it well in good sentences",username:"Ashley Wanjiru",author:'"Sylvia Plath"'},
+    {id:3,quote:"The first draft is just you telling yourself the story",username:"Stephen Kariuki",author:'"Terry Pratchett"'},
+    {id:4,quote:"Start before you’re ready",username:"Mary Wangui",author:'"Steven Pressfield"'},
+    {id:5,quote:"If you have no critics, you’ll likely have no success",username:"Joseph Kuria",author:'"Malcolm X"'},
   ];
   constructor() { }
 
