592841ebb523067d2a66f2a833d0d0fcb38bf88e
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-28 14:48:47 +0300

    added upvotes and downvotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,13 @@
 export class Quote {
-    showMore!: boolean
-    constructor(public id: number, public quote: string, public username: string, public author: string){
+    showMore!: boolean;
+    upVotes!: number;
+    downVotes!: number
+    date: Date;
+    constructor(public id: number, public quote: string, public username: string, public author: string,
+        public dte:Date){
         this.showMore = false;
+        this.downVotes = 0;
+        this.upVotes = 0;
+        this.date = dte;
     }
 }
