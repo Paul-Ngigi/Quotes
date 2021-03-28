9b2ddc2e9afdd83f240055af8aee4fab3c956343
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-28 14:48:07 +0300

    added a timeSince pipe

---
+++ src/app/time-since.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeSince'
+})
+export class TimeSincePipe implements PipeTransform {
+
+  seconds!:number;
+  minutes!:number;
+  hours!:number;
+  days!:number;
+  transform(date:Date): String {
+    this.seconds = new Date().getSeconds() - date.getSeconds();
+    this.minutes = new Date().getMinutes() - date.getMinutes();
+    this.hours = new Date().getHours() - date.getHours();
+    this.days = date.getDay() - new Date().getDay() ;
+
+    return `${this.days} days, ${this.hours} hours, ${this.minutes} minutes, ${this.seconds} seconds ago`;
+  }
+}
